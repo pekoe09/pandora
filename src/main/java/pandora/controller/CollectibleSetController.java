36202829e1fc8a3be34ac766cb3e693db77f3734
Juha <juha.kangas@welho.com>
2016-07-25 20:03:28 +0300

    Started on adding a set under a collection

--- src/main/java/pandora/controller/CollectibleSetController.java
+++ src/main/java/pandora/controller/CollectibleSetController.java
@@ -1,10 +1,72 @@
 package pandora.controller;
 
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pandora.domain.CollectibleCollection;
+import pandora.domain.CollectibleSet;
+import pandora.domain.User;
+import pandora.service.CollectibleCollectionService;
+import pandora.service.CollectibleSetService;
+import pandora.service.UserService;
 
 @Controller
-@RequestMapping("/setti")
+@RequestMapping("/setit")
 public class CollectibleSetController {
     
+    @Autowired
+    private CollectibleSetService collectibleSetService;
+    @Autowired
+    private CollectibleCollectionService collectibleCollectionService;
+    @Autowired
+    private UserService userService;
+    
+    @RequestMapping(value = "/lisaa", method = RequestMethod.GET)
+    public String create(
+            Model model,
+            @ModelAttribute CollectibleSet collectibleSet,
+            @RequestParam(required = true) Long collectibleCollectionId,
+            @RequestParam(required = false) Long parentSetId) {
+        User currentUser = userService.getCurrentUser();
+        CollectibleCollection collectibleCollection = null;
+        CollectibleSet parentSet = null;
+        try {
+            collectibleCollection = collectibleCollectionService.findOne(collectibleCollectionId, currentUser);
+            if(parentSetId != null) {
+                parentSet = collectibleSetService.findOne(parentSetId, currentUser);
+            }
+        } catch (IllegalArgumentException exc) {
+            return "kokoelma/" + collectibleCollectionId + 
+                    ((parentSetId != null && parentSet != null) ? ("/" + parentSetId) : null);
+        }
+        model.addAttribute("collectibleCollection", collectibleCollection);
+        model.addAttribute("parentSet", parentSet);
+        return "collectibleset_add";
+    }
+    
+    @RequestMapping(value = "/lisaa", method = RequestMethod.POST)
+    public String add(
+            Model model,
+            @Valid @ModelAttribute CollectibleSet collectibleSet,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()) {
+            return "collection_add";
+        }
+        User currentUser = userService.getCurrentUser();
+        try {
+            collectibleSet = collectibleSetService.save(collectibleSet, currentUser);
+        } catch (IllegalArgumentException exc) {
+            return "collection_add";
+        }
+        return "redirect:/kokoelmat/" + collectibleSet.getCollectibleCollection().getId() +
+                (collectibleSet.getParentSet() != null ? ("/" + collectibleSet.getParentSet().getId()) : null);
+    }
 }
--- src/main/java/pandora/service/CollectibleSetService.java
+++ src/main/java/pandora/service/CollectibleSetService.java
@@ -2,6 +2,8 @@ package pandora.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pandora.domain.CollectibleSet;
+import pandora.domain.User;
 import pandora.repository.CollectibleSetRepository;
 
 @Service
@@ -9,5 +11,13 @@ public class CollectibleSetService {
     
     @Autowired
     private CollectibleSetRepository collectibleSetRepository;
+
+    public CollectibleSet save(CollectibleSet collectibleSet, User currentUser) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public CollectibleSet findOne(Long parentSetId, User currentUser) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }

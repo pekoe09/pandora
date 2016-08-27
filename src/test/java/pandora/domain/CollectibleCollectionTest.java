ca66ff7da170e5424e4369723e6d96526e5f478f
Juha <juha.kangas@welho.com>
2016-08-27 19:25:59 +0300

    Added travis.yml

---
+++ src/test/java/pandora/domain/CollectibleCollectionTest.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pandora.domain;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author juha
+ */
+public class CollectibleCollectionTest {
+    
+    public CollectibleCollectionTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getName method, of class CollectibleCollection.
+     */
+    @Test
+    public void testGetName() {
+
+    }
+
+    /**
+     * Test of setName method, of class CollectibleCollection.
+     */
+    @Test
+    public void testSetName() {
+
+    }
+
+    /**
+     * Test of getCollectibleSets method, of class CollectibleCollection.
+     */
+    @Test
+    public void testGetCollectibleSets() {
+
+    }
+
+    /**
+     * Test of setCollectibleSets method, of class CollectibleCollection.
+     */
+    @Test
+    public void testSetCollectibleSets() {
+
+    }
+
+    /**
+     * Test of getUser method, of class CollectibleCollection.
+     */
+    @Test
+    public void testGetUser() {
+
+    }
+
+    /**
+     * Test of setUser method, of class CollectibleCollection.
+     */
+    @Test
+    public void testSetUser() {
+
+    }
+    
+}
